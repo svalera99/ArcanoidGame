ba249452b4d925b901b9b34afc9300c384499b48
svalera99 <34769034+svalera99@users.noreply.github.com>
2019-09-06 11:00:15 +0300

    -

---
+++ ThirdReadMe.txt
@@ -0,0 +1,1 @@
+Added level selection, fixed speed issues, fixed ball falling out issue.
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const canvasHeight = 600;
 const platform_w = 100;
 
 let playable = true, onGround = true, bricksInRow = 8, bricksInCol = 5, ballRadius = 15;
-draw_bricks_square = () =>{
+const draw_bricks_square = () =>{
     let bricks = [];
     let brick_w = 90, brick_h = 30;
     let render_x = 5, render_y = 10;
@@ -23,7 +23,27 @@ draw_bricks_square = () =>{
     }
     return bricks;
 };
-
+const draw_bricks_ladder = () =>{
+    let bricks = [];
+    let brick_w = 90, brick_h = 30;
+    let render_y = 10;
+    let padding = 5;
+    for (let i = 0; i < bricksInCol; ++i){
+        let canvas_slice = (canvasWidth - bricksInRow * (brick_w + padding) ) / 2;
+        bricks[i] = [];
+        for (let j = bricksInRow; j > 0; --j){
+            ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16); // random color generator
+            let brick = new Brick(canvas_slice, render_y,brick_h,brick_w);
+            brick.render(ctx);
+            bricks[i].push(brick);
+            canvas_slice += padding + brick_w;
+        }
+        bricksInRow-=2;
+        if (bricksInRow === 0) bricksInRow = 1;
+        render_y += 40;
+    }
+    return bricks;
+};
 class Brick{
     constructor(x,y,h,w){
         this.x = x;
@@ -41,12 +61,23 @@ class Brick{
         ctx.clearRect(this.x,this.y,this.w,this.h);
     }
 }
-let bricks = draw_bricks_square();
+let methodSelection = prompt("Please enter which field you want to play. Available options are : 1. Square 2. Ladder. Select 1 or 2");
+let bricks;
+while (!bricks){
+	if(methodSelection === '1')
+		bricks = draw_bricks_square();
+	else if(methodSelection === '2')
+	   bricks = draw_bricks_ladder();
+	else
+		methodSelection = prompt("Please enter which field you want to play. Available options are : 1. Square 2. Ladder. Select 1 or 2");
+}
+
+
 class Ball{
     constructor(plat_x, plat_y){
         this.x = plat_x;
         this.y =  plat_y;
-        this.Xspeed = 3;
+        this.Xspeed = 10;
         this.Yspeed = 0;
         this.angle = 0;
     }
@@ -61,27 +92,30 @@ class Ball{
         if (e.keyCode === 37){
             if ( this.Xspeed > 0)
                 this.Xspeed *= -1;
-            this.move();
+            if (this.x > 20)
+                this.move();
         }
         if (e.keyCode === 39 ){
             if (this.Xspeed < 0)
                 this.Xspeed *= -1;
-            this.move();
+            if (this.x < canvasWidth - 30)
+                this.move();
         }
     }
     move(){
         ctx.clearRect(this.x - ballRadius - 1, this.y - ballRadius - 1,
             ballRadius * 2 + 2, ballRadius * 2 + 2);
-        if (Math.cos(this.angle) < 0.2)
-            this.angle = 60;
+        if (Math.cos(this.angle) < 0.4)
+            this.angle = 0;
         this.x += this.Xspeed * Math.cos(this.angle);
         this.y += this.Yspeed * Math.cos(this.angle);
         this.render();
     }
     coll_detect(){
         for (let i=0; i<bricksInCol; i++){
-            for (let j=0; j< bricksInRow; j++){
-                if (bricks[i][j].isAlive && bricks[i][j].x - 2 <= this.x && this.x <= bricks[i][j].x + bricks[i][j].w && this.y - ballRadius <= bricks[i][j].y + bricks[i][j].h){
+            for (let j=0; j< bricks[i].length; j++){
+                if (bricks[i][j].isAlive && bricks[i][j].x - 2 <= this.x && this.x <= bricks[i][j].x + bricks[i][j].w &&
+                                                this.y - ballRadius <= bricks[i][j].y + bricks[i][j].h + 10){
                     bricks[i][j].kill();
                     this.Yspeed *= -1;
                 }
@@ -99,17 +133,18 @@ class Platform{
     constructor(){
         this.x = 0.1*canvasWidth;
         this.y = 0.9*canvasHeight;
-        this.w = 100
+        this.w = 100;
+        this.h = 20;
     }
     draw_platform = () =>{
         ctx.beginPath();
         ctx.fillStyle = "#FF0000";
-        ctx.fillRect(this.x, this.y, this.w,20);
+        ctx.fillRect(this.x, this.y, this.w,this.h);
         ctx.closePath();
     };
 
-    move_platform = (e) =>{
-        ctx.clearRect(this.x - 1, this.y - 1, canvasWidth, canvasHeight);
+    move_platform = (e) => {
+        ctx.clearRect(this.x - 1, this.y - 1, canvasWidth + 1, canvasHeight + 1);
         if (e.keyCode === 37){
             if (this.x - 10 > 0)
                 this.x -= 10;
@@ -122,12 +157,12 @@ class Platform{
 }
 const game_loop = (e, ball, platform) =>{
     if (!playable){
+        document.location.reload();
         if(ball.y <= 0) alert("You won");
         else alert("You've lost");
-        document.location.reload();
     }
     ball.coll_detect();
-    if (platform.x <= ball.x &&  ball.x <= platform.x + platform.w && ball.y >= platform.y) {
+    if (platform.x <= ball.x &&  ball.x <= platform.x + platform.w && ball.y >= platform.y && ball.y <= platform.y + platform.h) {
         ball.Yspeed *= -1;
     }
     ball.move();
@@ -139,9 +174,10 @@ const play = () =>{
     ball.render();
     platform.draw_platform(ctx, platform_w);
     document.addEventListener("keydown", function (e) {
-            if (e.keyCode === 32 || e.keyCode === 13){
+            if (e.keyCode === 32 || e.keyCode === 13){ // when enter or space is pressed
                 onGround = false;
                 ball.angle =  -(Math.random() * (Math.PI / 2) + Math.PI / 4);
+                ball.Xspeed = 2;
                 ball.Yspeed = ball.Xspeed;
                 setInterval(()=>{game_loop(e,ball,platform)}, 10);
             }
